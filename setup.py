ba558732619bbccb12c96a21ed7c6c3226bf7da9
Cristopher Sánchez <geronimosanchezromero@gmail.com>
2024-08-01 10:51:59 -0500

    Add setup.py correction

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cli-calculator",
-    version="o.1",
+    version="0.1",
     packages=['calculator', 'tests'],
     install_requires=[
         'click'
